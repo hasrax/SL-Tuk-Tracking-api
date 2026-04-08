01309c029ed2f1cd1b3b10344bef0d8c84c27b2e
Hasra Gunawardena <hasragunawardena093@gmail.com>
2026-04-08 19:47:49 +0530

    added mongoose connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
+// src/server.js
 const app = require("./app");
 const env = require("./config/env");
+const { connectDb } = require("./config/db");
 
-app.listen(env.port, () => {
-  console.log(`API listening on port ${env.port}`);
-});
+async function startServer() {
+  try {
+    await connectDb();
+    app.listen(env.port, () => {
+      console.log(`API listening on port ${env.port}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  }
+}
+
+startServer();
