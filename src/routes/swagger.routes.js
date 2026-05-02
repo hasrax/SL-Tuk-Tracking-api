d8019cdc569588f9ed0ab684c6b4e653f1a9b206
hasrax <hasragunawardena093@gmail.com>
2026-05-02 19:05:13 +0530

    Fix swagger docs route to avoid redirect

--- src/routes/swagger.routes.js
+++ src/routes/swagger.routes.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("../config/swagger");
 
 const router = express.Router();
 
 router.use("/docs", swaggerUi.serve);
-
-router.get("/docs", (req, res, next) => {
-  try {
-    
-    const swaggerSpec = require("../config/swagger");
-    return swaggerUi.setup(swaggerSpec)(req, res, next);
-  } catch (err) {
-    console.error("Swagger generation failed:", err);
-    return res.status(500).json({
-      message: "Swagger generation failed",
-      error: err.message,
-    });
-  }
-});
+router.get("/docs", swaggerUi.setup(swaggerSpec));
+router.get("/docs/", swaggerUi.setup(swaggerSpec));
 
 module.exports = router;
