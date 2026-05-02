e29e3fb92acfac95365a21f68284efcc97437849
hasrax <hasragunawardena093@gmail.com>
2026-05-02 18:54:17 +0530

    lambda error fixes

--- src/routes/swagger.routes.js
+++ src/routes/swagger.routes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../config/swagger");
 
 const router = express.Router();
 
-router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.use("/docs", swaggerUi.serve);
+
+router.get("/docs", (req, res, next) => {
+  try {
+    
+    const swaggerSpec = require("../config/swagger");
+    return swaggerUi.setup(swaggerSpec)(req, res, next);
+  } catch (err) {
+    console.error("Swagger generation failed:", err);
+    return res.status(500).json({
+      message: "Swagger generation failed",
+      error: err.message,
+    });
+  }
+});
 
 module.exports = router;
