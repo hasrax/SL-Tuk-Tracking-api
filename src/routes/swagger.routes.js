1f0944d3341536798f45c9b0702e116eb701f63a
hasrax <hasragunawardena093@gmail.com>
2026-05-02 19:15:52 +0530

    changed the code to see logs

--- src/routes/swagger.routes.js
+++ src/routes/swagger.routes.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../config/swagger");
 
 const router = express.Router();
 
 router.use("/docs", swaggerUi.serve);
-router.get("/docs", swaggerUi.setup(swaggerSpec));
-router.get("/docs/", swaggerUi.setup(swaggerSpec));
+
+router.get("/docs", (req, res, next) => {
+  try {
+    const swaggerSpec = require("../config/swagger");
+    return swaggerUi.setup(swaggerSpec)(req, res, next);
+  } catch (err) {
+    console.error("Swagger generation failed:", err);
+    console.error(err.stack);
+    return res.status(500).json({
+      message: "Swagger generation failed",
+      error: err.message,
+    });
+  }
+});
+
+router.get("/docs/", (req, res, next) => {
+  try {
+    const swaggerSpec = require("../config/swagger");
+    return swaggerUi.setup(swaggerSpec)(req, res, next);
+  } catch (err) {
+    console.error("Swagger generation failed:", err);
+    console.error(err.stack);
+    return res.status(500).json({
+      message: "Swagger generation failed",
+      error: err.message,
+    });
+  }
+});
 
 module.exports = router;
